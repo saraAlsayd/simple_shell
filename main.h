292313fd0104112286e8b0f527ab8adf99c3729a
saraAlsayd <roraya2442@gmail.com>
2023-07-22 07:04:23 -0700

    fghjk

---
+++ main.h
@@ -0,0 +1,37 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* for system getline() */
+#define USE_GETLINE 0
+#define USE_STRTOK 0
+
+/* to read and write the buffers */
+#define BUF_FLUSH -1
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+
+/* convert_number() */
+#define CONVERT_LOWERCASE	1
+#define CONVERT_UNSIGNED	2
+
+/* to chaining command */
+#define CMD_NORM	0
+#define CMD_OR		1
+#define CMD_AND		2
+#define CMD_CHAIN	3
+
+#define HIST_FILE	".simple_shell_history"
+#define HIST_MAX	4096
+
+#endif
