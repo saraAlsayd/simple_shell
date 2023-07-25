2e3443fdde756780b5a3930ee9b76d97467b5af4
saraAlsayd <roraya2442@gmail.com>
2023-07-25 13:07:23 -0700

    solll

---
+++ exit.c
@@ -0,0 +1,32 @@
+#include "master.h"
+
+/**
+  * __exit - exits from shell prog.
+  * @info: arguments passed in shell.
+  * Return: inter will return.
+  */
+int __exit(info_t *info)
+{
+	char **args = info->tokens + 1;
+
+	if (*args)
+	{
+		if (_isnumber(*args) && atou(*args) <= INT_MAX)
+		{
+			info->status = atou(*args);
+		}
+		else
+		{
+			perrorl_default(*info->argv, info->lineno, *args,
+					*info->tokens, "Illegal number", NULL);
+			info->status = 2;
+
+			return (info->status);
+		}
+	}
+	if (info->file)
+		close(info->fileno);
+
+	exit(free_info(info));
+}
+
---
+++ unsetenv.c
@@ -0,0 +1,25 @@
+#include "master.h"
+
+/**
+  * __unsetenv - unseting the environment of the variable.
+  * @info: arguments passed in this.
+  * Return: status of the env.
+  */
+int __unsetenv(info_t *info)
+{
+	char **args = info->tokens + 1;
+
+	if (*args)
+	{
+		while (*args)
+			del_dict_node(&info->env, *args++);
+		info->status = EXIT_SUCCESS;
+	}
+	else
+	{
+		perrorl("Too few arguments.", *info->tokens, NULL);
+		info->status = EXIT_FAILURE;
+	}
+	return (info->status);
+}
+
